3daf7e2d621c626df5e2852d8ecb81aa19852ec0
matajar <info.matajar@gmail.com>
2021-09-07 13:27:53 +0400

    icon changed, made scrollable, add container

--- lib/main.dart
+++ lib/main.dart
@@ -10,70 +10,64 @@ void main() {
           title: Text('E-commerce'),
           backgroundColor: Colors.lightBlue[600],
         ),
-        body: new Container(
-          color: Color(0xff258DED),
-          height: 400.0,
-          alignment: Alignment.center,
-          child: new Column(
+        // CustomScrollView()
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              new Container(
-                height: 200.0,
-                width: 200.0,
-                decoration: new BoxDecoration(
-                    image: DecorationImage(
-                        image: new AssetImage('images/image-1.jpg'),
-                        fit: BoxFit.fill),
-                    shape: BoxShape.circle),
+              CarouselSlider(
+                options: CarouselOptions(
+                  // height: 250,
+                  aspectRatio: 16 / 9,
+                  viewportFraction: 0.8,
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  reverse: false,
+                  autoPlay: true,
+                  autoPlayInterval: Duration(seconds: 3),
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeCenterPage: true,
+                  scrollDirection: Axis.horizontal,
+                ),
+                items: [
+                  'images/image-1.jpg',
+                  'images/images-2.jpg',
+                ].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 2.0,
+                        ),
+                        child: Image(
+                          image: AssetImage('$i'),
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
               ),
-              new Container(
-                child: new Text(
-                  'Welcome to Prime Message',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontFamily: 'Aleo',
-                      fontStyle: FontStyle.normal,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25.0,
-                      color: Colors.white),
+              Container(
+                width: double.infinity,
+                height: 300,
+                child: Image.asset(
+                  'gif/gif-1.gif',
+                  fit: BoxFit.fill,
                 ),
               ),
+              Container(
+                width: double.infinity,
+                height: 300,
+                child: Image.asset(
+                  'gif/gif-2.gif',
+                  fit: BoxFit.fill,
+                ),
+              )
             ],
           ),
         ),
-        // CarouselSlider(
-        //   options: CarouselOptions(
-        //     height: 250,
-        //     aspectRatio: 16 / 9,
-        //     viewportFraction: 0.8,
-        //     initialPage: 0,
-        //     enableInfiniteScroll: true,
-        //     reverse: false,
-        //     autoPlay: true,
-        //     autoPlayInterval: Duration(seconds: 3),
-        //     autoPlayAnimationDuration: Duration(milliseconds: 800),
-        //     autoPlayCurve: Curves.fastOutSlowIn,
-        //     enlargeCenterPage: true,
-        //     scrollDirection: Axis.horizontal,
-        //   ),
-        //   items: [
-        //     'images/image-1.jpg',
-        //     'images/images-2.jpg',
-        //   ].map((i) {
-        //     return Builder(
-        //       builder: (BuildContext context) {
-        //         return Container(
-        //           width: MediaQuery.of(context).size.width,
-        //           margin: EdgeInsets.symmetric(
-        //             horizontal: 2.0,
-        //           ),
-        //           child: Image(
-        //             image: AssetImage('$i'),
-        //           ),
-        //         );
-        //       },
-        //     );
-        //   }).toList(),
-        // ),
       ),
     ),
   );
